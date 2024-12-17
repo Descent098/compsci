acb66444ae94ab6b4714bec54258db36ee8b1515
Kieran Wood <kieran@canadiancoding.ca>
2024-12-17 14:42:44 -0700

    Added V4.4 updates

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,35 +1,21 @@
-import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
+import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
-const imageFocusing = `document.querySelectorAll('img').forEach( 
-  (image)=>{
-      image.addEventListener('click', focusImage)
-  }
-)
-
-function focusImage(){
-  if (this.classList.contains('focusedImage')){
-      document.exitFullscreen()
-  }else{
-      this.requestFullscreen()
-  }
-  this.classList.toggle('focusedImage')
-}`
-
 export default ((opts?: Options) => {
-  function Footer({ displayClass }: QuartzComponentProps) {
+  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <hr />
         <p>
-          Created with <a href="https://quartz.jzhao.xyz/" target="_blank">Quartz v{version}</a>, © {year}
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
@@ -38,7 +24,6 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
-        <script>{imageFocusing}</script>
       </footer>
     )
   }
